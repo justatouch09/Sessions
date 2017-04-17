ba34f11421f6c0a247ff7e710137fc5cdead8ace
Jarad Touchberry <you@example.com>
2017-04-17 12:40:35 -0400

    working on code

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,49 @@
 package com.theironyard.charlotte;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+import sun.plugin2.message.Message;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
+public static User user;
+
+public static Message message;
+
     public static void main(String[] args) {
-	// write your code here
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    if (user == null) {
+                        return new ModelAndView(m, "login.html");
+                    } else {
+                        m.put("name", user.name);
+                        return new ModelAndView(m, "home.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                ((request, response) -> {
+                    String name = request.queryParams("loginName");
+                    user = new User(name);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    ArrayList<Message> messages = new ArrayList<>();
+                    message = new Message("message", user.message);
+                }
     }
 }
---
+++ src/com/theironyard/charlotte/User.java
@@ -0,0 +1,15 @@
+package com.theironyard.charlotte;
+
+/**
+ * Created by jaradtouchberry on 4/14/17.
+ */
+public class User {
+    String name;
+    String message;
+
+
+    public User(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+}
