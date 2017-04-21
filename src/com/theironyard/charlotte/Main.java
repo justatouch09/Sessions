5f04596aa0d9e242e93ddea8d792c3c5353b047a
Jarad Touchberry <you@example.com>
2017-04-21 15:58:47 -0400

    code touched

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -10,29 +10,11 @@ public class Main {
 
     public static HashMap<String, User> users = new HashMap<>();
 
-    public static String MESSAGES = "messages";
 
-    public String NameList = "name";
-
-    private boolean addMessageToUser(String userName, Message m, HashMap<String, User> users){
-        if (users == null || users.isEmpty()) return false;
-
-        for(Entry<String, User> entry : users.entrySet()) {
-            if (entry.key().equals(userName)) {
-                if (entry.value() != null && entry.value().messages != null) {
-                    entry.value().messages.add(m);
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
 
     public static void main(String[] args) {
         ///make sure each user has own coment of messages hasmap strring to messages;
         //each user object group of messages;
-
         Spark.get(
                 "/",
                 (request, response) -> {
@@ -46,30 +28,32 @@ public class Main {
                         return new ModelAndView(m, "login.html");
                     } else {
                         m.put("name", user.name);
-                        m.put("notes", MESSAGES);
+                        //when user logged in add name and message list to model
+                        m.put("notes", user.messageList);
                         return new ModelAndView(m, "home.html");
                     }
                 },
                 new MustacheTemplateEngine()
         );
 
-        Spark.post(
-                "/login",
-                (request, response) -> {
-                    String name = request.queryParams("nameFromForm");
-                    User user = new User(name);
-                    if (user == null) {
-                        user = new User(name);
-                        users.put(name, user);
-                    } else {
-                    }
-                    Session session = request.session();
-                    session.attribute("userName", name);
-
-                    response.redirect("/");
-                    return "";
-                }
-        );
+//        Spark.post(
+//                "/login",
+//                (request, response) -> {
+//                    String name = request.queryParams("loginName");
+//                    User user =
+//                    if (users.get(user) == null) {
+//                        user = new User(name);
+//
+//                        users.put(name, user);
+//                    } else {
+//                    }
+//                    Session session = request.session();
+//                    session.attribute("userName", name);
+//
+//                    response.redirect("/");
+//                    return "";
+//                }
+//        );
 
         Spark.post("/logout",
                 (request, response) -> {
@@ -79,26 +63,5 @@ public class Main {
                     return "";
                 }
         );
-//
-//        Spark.get("new-message",
-//                (request, response) -> {
-//                    return new ModelAndView(null, "message.html");
-//                },
-//                new MustacheTemplateEngine();
-//        );
-//
-//        Spark.post(
-//                "/messages",
-//                (request, response) -> {
-//                    String message = request.queryParams("message-text");
-//                    Message Mk = new Message(message);
-//                    MESSAGES.add(Mk);
-//                    response.redirect("/");
-//                    return "";
-//                    //redirect to root
-//                }
-//        );
-//    }
-//}
     }
 }
